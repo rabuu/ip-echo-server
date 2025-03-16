d22155e88d8852ec2ecfd95e432cc91cf8060a4e
Rasmus Buurman <rasmus@rbuurman.de>
2025-03-16 17:41:56 +0100

    Implement simple echo IP app using Express

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080;
+
+app.set('trust proxy', true)
+
+app.get("/", (req, res) => {
+	console.log(`GET request by ${req.ip}`)
+	res.set('Content-Type', 'text/plain')
+	res.send(req.ip)
+})
+
+app.listen(port, () => {
+	console.log(`App is listening on port ${port}...`);
+})
